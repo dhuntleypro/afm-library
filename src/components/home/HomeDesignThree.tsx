ab3b5c3726be36371f9636b12c8ef93cfdadcde2
Darrien Huntley <dhuntleypro@icloud.com>
2024-09-02 08:07:53 -0400

    number jump from 0.1.21

---
+++ src/components/home/HomeDesignThree.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeDesignThree = () => {
+  return (
+    <View>
+      <Text>HomeDesignThree</Text>
+    </View>
+  )
+}
+
+export default HomeDesignThree
+
+const styles = StyleSheet.create({})
